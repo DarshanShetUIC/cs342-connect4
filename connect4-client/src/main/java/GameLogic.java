66b3917ee614a0e3de813bc8c238feb433b5691d
david-dozer <dgmendoza1015@gmail.com>
2022-11-26 17:10:20 -0600

    new imp. for win

--- connect4-client/src/main/java/GameLogic.java
+++ connect4-client/src/main/java/GameLogic.java
@@ -18,37 +18,39 @@ public class GameLogic {
 		return boardMatrix;
 	}
 	
-	public static boolean win(CFourInfo data) {
-		// horizontalCheck 
-	    for (int j = 0; j< 6-3; j++ ){
-	        for (int i = 0; i<7; i++){
-	            if (data.boardMatrix[i][j] == data.playerTurn && data.boardMatrix[i][j+1] == data.playerTurn && data.boardMatrix[i][j+2] == data.playerTurn && data.boardMatrix[i][j+3] == data.playerTurn){
+	public static boolean checkWin(CFourInfo data) {
+	    final int HEIGHT = 6;
+	    final int WIDTH = 7;
+	    final int EMPTY_SLOT = 0;
+	    for (int r = 0; r < HEIGHT; r++) { // iterate rows, bottom to top
+	        for (int c = 0; c < WIDTH; c++) { // iterate columns, left to right
+	            int player = data.boardMatrix[r][c];
+	            if (player == EMPTY_SLOT)
+	                continue; // don't check empty slots
+
+	            if (c + 3 < WIDTH &&
+	                player == data.boardMatrix[r][c+1] && // look right
+	                player == data.boardMatrix[r][c+2] &&
+	                player == data.boardMatrix[r][c+3])
 	                return true;
-	            }           
+	            if (r + 3 < HEIGHT) {
+	                if (player == data.boardMatrix[r+1][c] && // look up
+	                    player == data.boardMatrix[r+2][c] &&
+	                    player == data.boardMatrix[r+3][c])
+	                    return true;
+	                if (c + 3 < WIDTH &&
+	                    player == data.boardMatrix[r+1][c+1] && // look up & right
+	                    player == data.boardMatrix[r+2][c+2] &&
+	                    player == data.boardMatrix[r+3][c+3])
+	                    return true;
+	                if (c - 3 >= 0 &&
+	                    player == data.boardMatrix[r+1][c-1] && // look up & left
+	                    player == data.boardMatrix[r+2][c-2] &&
+	                    player == data.boardMatrix[r+3][c-3])
+	                    return true;
+	            }
 	        }
 	    }
-	    // verticalCheck
-	    for (int i = 0; i<6-3; i++ ){
-	        for (int j = 0; j<7; j++){
-	            if (data.boardMatrix[i][j] == data.playerTurn && data.boardMatrix[i+1][j] == data.playerTurn && data.boardMatrix[i+2][j] == data.playerTurn && data.boardMatrix[i+3][j] == data.playerTurn){
-	                return true;
-	            }           
-	        }
-	    }
-	    // ascendingDiagonalCheck 
-	    for (int i=3; i<6; i++){
-	        for (int j=0; j<7-3; j++){
-	            if (data.boardMatrix[i][j] == data.playerTurn && data.boardMatrix[i-1][j+1] == data.playerTurn && data.boardMatrix[i-2][j+2] == data.playerTurn && data.boardMatrix[i-3][j+3] == data.playerTurn)
-	                return true;
-	        }
-	    }
-	    // descendingDiagonalCheck
-	    for (int i=3; i<6; i++){
-	        for (int j=3; j<7; j++){
-	            if (data.boardMatrix[i][j] == data.playerTurn && data.boardMatrix[i-1][j-1] == data.playerTurn && data.boardMatrix[i-2][j-2] == data.playerTurn && data.boardMatrix[i-3][j-3] == data.playerTurn)
-	                return true;
-	        }
-	    }
-	    return false;
-	}	
+	    return false; // no winner found
+	}
 }
